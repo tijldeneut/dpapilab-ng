a9fb46eab905dde5982ebfa6b0806c7a7b003b77
Tijl Deneut <deneut_tijl@hotmail.com>
2021-09-30 12:21:46 +0200

    Add files via upload
    file rename

---
+++ mkdecs.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015, Francesco "dfirfpi" Picasso <francesco.picasso@gmail.com>
+# Copyright 2020, Photubias <tijl.deneut@howest.be>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" Windows DPAPI system's MasterKeys decryption utility."""
+
+import hashlib, optparse, os, re, sys
+
+try:
+    from dpapick3 import masterkey, registry
+except ImportError:
+    raise ImportError('Missing dpapick3, please install via pip install dpapick3.')
+
+def check_parameters(options, args):
+    """Simple checks on the parameters set by the user."""
+    if not options.security or not options.system:
+        sys.exit('You must provide SYSTEM and SECURITY hives.')
+    if not args:
+        sys.exit('You must provide at least one MasterKey file.')
+
+def parseGUID(bData):
+    def reverseByte(bByteInput):
+        sReversed = ''
+        sHexInput = bByteInput.hex()
+        for x in range(-1, -len(str(sHexInput)), -2): sReversed += sHexInput[x-1] + sHexInput[x]
+        return bytes.fromhex(sReversed)
+    return reverseByte(bData[:4]).hex() + '-' + reverseByte(bData[4:6]).hex() + '-' + reverseByte(bData[6:8]).hex() + '-' + bData[8:10].hex() + '-' + bData[10:].hex()
+
+if __name__ == '__main__':
+    """Utility core."""
+    usage = (
+        'usage: %prog [options] MKfile1 MKfile2 etc.\n\n'
+        'e.g. Windows\\System32\\Microsoft\\Protect\\S-1-5-18\\User\\*\n'
+        'It tries to unlock (decrypt) *system* MasterKey files provided.')
+
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option('--system', metavar='HIVE', dest='system')
+    parser.add_option('--security', metavar='HIVE', dest='security')
+
+    (options, args) = parser.parse_args()
+
+    check_parameters(options, args)
+
+    reg = registry.Regedit()
+    secrets = reg.get_lsa_secrets(options.security, options.system)
+    dpapi_system = secrets.get('DPAPI_SYSTEM')['CurrVal']
+
+    mkp = masterkey.MasterKeyPool()
+    mkp.addSystemCredential(dpapi_system)
+
+    for arg in args:
+        if os.path.isfile(arg):
+            with open(arg,'rb') as f:
+                try: mkp.addMasterKey(f.read())
+                except: pass
+        else:
+            for file in os.listdir(arg.replace('*','')):
+                filepath = os.path.join(arg.replace('*',''),file)
+                if not os.path.isfile(filepath): break
+                with open(filepath, 'rb') as f:
+                    if file == 'Preferred': print('[+] Preffered Key is ' + parseGUID(f.read())[:36])
+                    try: mkp.addMasterKey(f.read())
+                    except: pass
+
+    mkp.try_credential_hash(None, None)
+
+    for mkl in list(mkp.keys.values()):
+        for mk in mkl:
+            print('')
+            print(('[!] Working on MK GUID %s\n-------------' % mk.guid.decode()))
+            if mk.decrypted:
+                print('[+] MASTER KEY UNLOCKED!')
+                mkey = mk.get_key()
+                print(('[+] KEY: %s' % mkey.hex()))
+                print(('[+] SHA1: %s' % hashlib.sha1(mkey).digest().hex()))
+            else:
+                print(('[-] MK guid: %s' % mk.guid))
+                print('[-] UNABLE to UNLOCK master key')
+            print('')
---
+++ mkdecu.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015, Francesco "dfirfpi" Picasso <francesco.picasso@gmail.com>
+# Copyright 2021, Photubias <tijl.deneut@howest.be>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+## Also works with Domain PVK file (PriVateKey), 
+##  Get it from the PDC using PowerShell:
+##   "Install-Module -Name dsinternals -force; Import-Module DSInternals"
+##   "Get-ADReplBackupKey -Server 192.168.1.1 -Credential (Get-Credential) | Save-DPAPIBlob -DirectoryPath ."
+""" Windows DPAPI user's MasterKeys decryption utility."""
+
+import optparse, sys, re
+
+try:
+    from dpapick3 import masterkey
+except ImportError:
+    raise ImportError('[-] Missing dpapick3, please install via pip install dpapick3.')
+
+def check_parameters(options, args):
+    """Simple checks on the parameters set by the user."""
+    if not options.sid:
+        try:
+            options.sid = re.findall(r"S-1-\d+-\d+-\d+-\d+-\d+-\d+", args[0])[0]
+            print('[+] Detected SID: ' + options.sid)
+        except:
+            sys.exit('[-] You must provide the user\'s SID textual string.')
+    if not options.password and not options.pwdhash and not options.pvk:
+        print('[!] No password provided, assuming user has no password.')
+        options.pwdhash = 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
+        #options.pwdhash = '31d6cfe0d16ae931b73c59d7e0c089c0' ## NT hash sometimes also works
+    if not args:
+        sys.exit('[-] You must provide at least one MasterKey file.')
+
+if __name__ == '__main__':
+    """Utility core."""
+    usage = (
+        'usage: %prog [options] MKfile1 MKfile2 etc.\n\n'
+        'It tries to unlock (decrypt) *user* MasterKey files provided.\n'
+        ' Default User MK location: %localappdata%/Roaming/Microsoft/Protect')
+
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option('--sid', '-s', help='Optional: will try to construct from MK path when not provided')
+    parser.add_option('--credhist', '-c', help='Optional: Add credhist file')
+    parser.add_option('--password', '-p', help='Optional: Will use empty password when not provided')
+    parser.add_option('--pwdhash', '-a', help='Optional: Depending on MK; either a SHA1 or NT password hash')
+    parser.add_option('--pvk', '-k', help='Optional: Depending on MK; domain RSA PVK keyfile')
+
+    (options, args) = parser.parse_args()
+
+    check_parameters(options, args)
+
+    oMKP = masterkey.MasterKeyPool()
+    for arg in args:
+        try: oMKP.loadDirectory(arg.replace('*',''))
+        except: pass
+        try: oMKP.addMasterKey(open(arg,'rb').read())
+        except: pass
+    print('[!] Imported {} keys'.format(str(len(list(oMKP.keys)))))
+    print('    Now adding credential(s), hold on as this may take some time')
+    if options.credhist: oMKP.addCredhistFile(options.sid, options.credhist)
+    if options.password: oMKP.try_credential(options.sid, options.password)
+    if options.pwdhash: oMKP.try_credential_hash(options.sid, bytes.fromhex(options.pwdhash))
+    if options.pvk: oMKP.try_domain(options.pvk)
+    
+    iDecrypted = 0
+    for oMKGUID in list(oMKP.keys):
+        oMK = oMKP.getMasterKeys(oMKGUID)[0]
+        if oMK.decrypted:
+            iDecrypted+=1
+            print('[+] MK decrypted: {}'.format(oMKGUID.decode(errors='ignore')))
+            print('    Secret: ' + oMK.get_key().hex())
+    if iDecrypted > 0: print('[+] Success. Decrypted {} out of {} keys'.format(str(iDecrypted), str(len(list(oMKP.keys)))))
+    else: print('[-] No luck, try a different approach (NT hash, lsass dump, cleartext password, PVK AD cert, credhist file ...)')
